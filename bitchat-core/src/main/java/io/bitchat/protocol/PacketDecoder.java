59a38700c57aeb0f0407ad28424e52032c92f731
all4you <wh_all4you@hotmail.com>
2019-10-24 09:36:13 +0800

    bug fix

--- bitchat-core/src/main/java/io/bitchat/protocol/PacketDecoder.java
+++ bitchat-core/src/main/java/io/bitchat/protocol/PacketDecoder.java
@@ -28,11 +28,11 @@ public class PacketDecoder extends LengthFieldBasedFrameDecoder {
 
     private SerializerChooser chooser;
 
-    public final int MAX_FRAME_LENGTH = Integer.MAX_VALUE;
+    public static final int MAX_FRAME_LENGTH = Integer.MAX_VALUE;
 
-    public final int LENGTH_FIELD_OFFSET = 3;
+    public static final int LENGTH_FIELD_OFFSET = 3;
 
-    public final int LENGTH_FIELD_LENGTH = 4;
+    public static final int LENGTH_FIELD_LENGTH = 4;
 
     public PacketDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength) {
         super(maxFrameLength, lengthFieldOffset, lengthFieldLength);
@@ -40,33 +40,36 @@ public class PacketDecoder extends LengthFieldBasedFrameDecoder {
     }
 
     @Override
-    protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
+    protected Packet decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
         // get the whole tcp package
         // decoded by super class
-        in = (ByteBuf) super.decode(ctx, in);
+        ByteBuf decodedIn = (ByteBuf) super.decode(ctx, in);
+        if (decodedIn == null) {
+            return null;
+        }
 
         // check the header length
         int leastPacketLen = LENGTH_FIELD_OFFSET + LENGTH_FIELD_LENGTH;
-        if (in.readableBytes() < leastPacketLen) {
+        if (decodedIn.readableBytes() < leastPacketLen) {
             throw new DecoderException("packet header length less than leastPacketLen=" + leastPacketLen);
         }
 
-        byte magic = in.readByte();
-        byte algorithm = in.readByte();
+        byte magic = decodedIn.readByte();
+        byte algorithm = decodedIn.readByte();
         Serializer serializer = chooser.choose(algorithm);
         if (serializer == null) {
             throw new DecoderException("serialize algorithm is invalid");
         }
 
-        byte type = in.readByte();
-        int len = in.readInt();
-        if (in.readableBytes() != len) {
+        byte type = decodedIn.readByte();
+        int len = decodedIn.readInt();
+        if (decodedIn.readableBytes() != len) {
             throw new DecoderException("packet content marked length is not equals to actual length");
         }
 
         // read content
         byte[] content = new byte[len];
-        in.readBytes(content);
+        decodedIn.readBytes(content);
 
         return serializer.deserialize(content, DefaultPacket.class);
     }
