9692bdc5ceb50503c3d2e4e2938afafad1b257d9
all4you <wh_all4you@hotmail.com>
2020-01-13 22:06:33 +0800

    修改SessionManager相关接口的方法

--- bitchat-server/src/main/java/io/bitchat/server/session/AbstractSessionManager.java
+++ bitchat-server/src/main/java/io/bitchat/server/session/AbstractSessionManager.java
@@ -3,7 +3,9 @@ package io.bitchat.server.session;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.lang.Assert;
 import cn.hutool.core.util.IdUtil;
-import io.bitchat.server.channel.ChannelType;
+import io.bitchat.server.channel.ChannelManager;
+import io.bitchat.server.channel.ChannelWrapper;
+import io.bitchat.server.channel.DefaultChannelManager;
 import io.netty.channel.ChannelId;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,8 +22,11 @@ public abstract class AbstractSessionManager implements SessionManager {
 
     private Map<String, Session> sessionMap;
 
+    private ChannelManager channelManager;
+
     public AbstractSessionManager() {
         sessionMap = new ConcurrentHashMap<>();
+        channelManager = DefaultChannelManager.getInstance();
     }
 
     public abstract Session newSession(String sessionId);
@@ -35,10 +40,12 @@ public abstract class AbstractSessionManager implements SessionManager {
     }
 
     @Override
-    public void bound(Session session, ChannelId channelId, ChannelType channelType) {
+    public void bound(Session session, ChannelId channelId) {
         Assert.notNull(session, "session can not be null");
         Assert.notNull(channelId, "channelId can not be null");
-        session.bound(channelId, channelType);
+        ChannelWrapper channelWrapper = channelManager.getChannelWrapper(channelId);
+        Assert.notNull(channelWrapper, "channelId does not exists");
+        session.bound(channelId, channelWrapper.getChannelType());
     }
 
     @Override
--- bitchat-server/src/main/java/io/bitchat/server/session/SessionManager.java
+++ bitchat-server/src/main/java/io/bitchat/server/session/SessionManager.java
@@ -1,6 +1,5 @@
 package io.bitchat.server.session;
 
-import io.bitchat.server.channel.ChannelType;
 import io.netty.channel.ChannelId;
 
 import java.util.Collection;
@@ -12,7 +11,7 @@ public interface SessionManager {
 
     Session newSession();
 
-    void bound(Session session, ChannelId channelId, ChannelType channelType);
+    void bound(Session session, ChannelId channelId);
 
     void removeSession(ChannelId channelId);
 
