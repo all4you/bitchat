853c90f4a2851b8bd52a8d16cdfa8eb99201000a
all4you <wh_all4you@hotmail.com>
2020-01-17 22:04:34 +0800

    有新用户登录时，广播通知其他用户

--- bitchat-im/bitchat-im-core/src/main/java/io/bitchat/im/ImServiceName.java
+++ bitchat-im/bitchat-im-core/src/main/java/io/bitchat/im/ImServiceName.java
@@ -10,5 +10,6 @@ public interface ImServiceName {
     String GET_ONLINE_USER = "getOnlineUser";
     String SEND_P2P_MSG = "sendP2PMsg";
     String TRANSFER_MSG = "transferMsg";
+    String NEW_USER_LOGIN = "newUserLogin";
 
 }
--- bitchat-im/bitchat-im-server/src/main/java/io/bitchat/im/server/processor/login/LoginProcessor.java
+++ bitchat-im/bitchat-im-server/src/main/java/io/bitchat/im/server/processor/login/LoginProcessor.java
@@ -9,19 +9,28 @@ import io.bitchat.im.server.session.ImSession;
 import io.bitchat.im.server.session.ImSessionManager;
 import io.bitchat.im.user.User;
 import io.bitchat.lang.constants.ResultCode;
+import io.bitchat.packet.Command;
+import io.bitchat.packet.Packet;
 import io.bitchat.packet.Payload;
+import io.bitchat.packet.factory.CommandFactory;
+import io.bitchat.packet.factory.PacketFactory;
 import io.bitchat.packet.factory.PayloadFactory;
 import io.bitchat.packet.processor.AbstractRequestProcessor;
 import io.bitchat.packet.processor.Processor;
 import io.bitchat.server.ServerAttrHolder;
 import io.bitchat.server.channel.ChannelHelper;
 import io.bitchat.server.channel.ChannelType;
+import io.bitchat.server.session.Session;
 import io.bitchat.server.session.SessionHelper;
 import io.bitchat.server.session.SessionManager;
+import io.bitchat.ws.Frame;
+import io.bitchat.ws.FrameFactory;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -71,7 +80,49 @@ public class LoginProcessor extends AbstractRequestProcessor {
         // bound the session with channelId
         sessionManager.bound(imSession, channel.id(), user.getUserId());
         SessionHelper.markOnline(channel, imSession.sessionId());
+        // broadcast other users
+        broadcastNewUserLogin(imSession);
     }
 
+    private void broadcastNewUserLogin(ImSession newUserSession) {
+        List<Session> onlineUsers = sessionManager.getAllSessions();
+        Long userId = newUserSession.userId();
+        String userName = newUserSession.getUserName();
+        for (Session session : onlineUsers) {
+            if (session.sessionId().equals(newUserSession.sessionId())) {
+                continue;
+            }
+            Object transferMsg;
+            if (session.channelType() == ChannelType.Packet) {
+                transferMsg = buildPacket(userId, userName);
+            } else {
+                transferMsg = buildFrame(userId, userName);
+            }
+            session.writeAndFlush(transferMsg);
+        }
+    }
+
+    /**
+     * build transfer msg packet
+     */
+    private Packet buildPacket(Long userId, String userName) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("userId", userId);
+        params.put("userName", userName);
+        Command transferMsgCommand = CommandFactory.newCommand(ImServiceName.NEW_USER_LOGIN, params);
+        // create a new command packet
+        return PacketFactory.newCmdPacket(transferMsgCommand);
+    }
+
+    /**
+     * build transfer msg frame
+     */
+    private Frame buildFrame(Long userId, String userName) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("userId", userId);
+        params.put("userName", userName);
+        // create a new command frame
+        return FrameFactory.newCmdFrame(ImServiceName.NEW_USER_LOGIN, params);
+    }
 
 }
