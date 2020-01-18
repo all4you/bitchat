3384ce25a6872859620044c4a67f738ce9622d59
all4you <wh_all4you@hotmail.com>
2020-01-18 15:46:11 +0800

    将bitchat-im移到单独的项目中去

--- bitchat-im/bitchat-im-server/src/main/java/io/bitchat/im/server/dao/user/BcUserMapper.java
+++
@@ -1,8 +0,0 @@
-package io.bitchat.im.server.dao.user;
-
-import io.bitchat.im.server.entity.user.BcUserDO;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface BcUserMapper extends Mapper<BcUserDO> {
-
-}
